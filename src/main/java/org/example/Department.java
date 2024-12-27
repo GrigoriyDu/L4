88ea6cd7c5b722d7c537f7a79884d42fa28022a1
Grigorii Dukach <dukach055@yandex.ru>
2024-12-27 08:44:04 +0300

    Написание конструктора для класса Department

--- src/main/java/org/example/Department.java
+++ src/main/java/org/example/Department.java
@@ -1,9 +1,22 @@
 package org.example;
 
+import java.util.UUID;
+
 /**
  * Класс, представляющий департамент.
  */
 public class Department {
     private String departmentName;
     private UUID departmentID;
+
+    /**
+     * Конструктор для создания объекта Department.
+     *
+     * @param departmentName Название департамента.
+     * @param departmentID   Уникальный идентификатор департамента.
+     */
+    public Department(String departmentName, UUID departmentID) {
+        this.departmentName = departmentName;
+        this.departmentID = departmentID;
+    }
 }
