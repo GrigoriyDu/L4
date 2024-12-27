db306d1123652deb8053840a7b635073e3599152
Grigorii Dukach <dukach055@yandex.ru>
2024-12-27 08:38:50 +0300

    Добавление геттеров и переопределение toString для класса Person

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -29,4 +29,68 @@ public class Person {
         this.salary = salary;
         this.department = department;
     }
+
+    /**
+     * Возвращает уникальный идентификатор человека.
+     *
+     * @return Уникальный идентификатор человека.
+     */
+    public int getPersonID() {
+        return personID;
+    }
+
+    /**
+     * Возвращает имя человека.
+     *
+     * @return Имя человека.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Возвращает пол человека.
+     *
+     * @return Пол человека.
+     */
+    public String getSex() {
+        return sex;
+    }
+
+    /**
+     * Возвращает дату рождения человека.
+     *
+     * @return Дата рождения человека.
+     */
+    public String getBirthday() {
+        return birthday;
+    }
+
+    /**
+     * Возвращает департамент, к которому принадлежит человек.
+     *
+     * @return Департамент, к которому принадлежит человек.
+     */
+    public Department getDepartment() {
+        return department;
+    }
+
+    /**
+     * Возвращает зарплату человека.
+     *
+     * @return Зарплата человека.
+     */
+    public int getSalary() {
+        return salary;
+    }
+
+    /**
+     * Возвращает строковое представление объекта Person.
+     *
+     * @return Строковое представление объекта Person.
+     */
+    @Override
+    public String toString() {
+        return "Person{personID=" + personID + ", name='" + name + "', sex='" + sex + "', birthday='" + birthday + "', department=" + department + ", salary=" + salary + "}";
+    }
 }
